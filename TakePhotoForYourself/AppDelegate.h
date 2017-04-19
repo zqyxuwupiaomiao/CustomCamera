30b6169730bcbe2876485556becf4b6ffd62fcbc
zqyxuwupiaomiao <809183219@qq.com>
2017-04-19 10:42:00 +0800

    Initial Commit

---
+++ TakePhotoForYourself/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TakePhotoForYourself
+//
+//  Created by zqy on 2017/4/19.
+//  Copyright © 2017年 zqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
